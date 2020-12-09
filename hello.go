bcbe8abfd3c904c6ffd7b70505a604785bcc2f33
Spencer17x <1253478653@qq.com>
2020-12-09 22:40:37 +0800

    feat: grade 函数

--- hello.go
+++ hello.go
@@ -1,11 +1,35 @@
 package main
 
-import (
-	"fmt"
-	"math/cmplx"
-)
+import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
-	fmt.Println(cmplx.Abs(3 + 4i))
+	fmt.Print(
+		grade(0),
+		grade(50),
+		grade(60),
+		grade(70),
+		grade(80),
+		grade(90),
+		grade(100),
+		//grade(110),
+	)
+}
+
+func grade(score int) string {
+	result := ""
+	switch {
+	case score <= 60:
+		result = "E"
+	case score <= 70:
+		result = "D"
+	case score <= 80:
+		result = "C"
+	case score <= 90:
+		result = "B"
+	case score <= 100:
+		result = "A"
+	default:
+		panic("找不到匹配值")
+	}
+	return result
 }
