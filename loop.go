87b043bd2e4cb101c76e03bf3b660cd5e99b04e2
Spencer17x <1253478653@qq.com>
2021-04-26 23:54:26 +0800

    feat: go func

--- loop.go
+++ loop.go
@@ -1,38 +1,38 @@
-package main
+// package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-	"strconv"
-)
+// import (
+// 	"bufio"
+// 	"fmt"
+// 	"os"
+// 	"strconv"
+// )
 
-func convertToBin(n int) string {
-	result := ""
-	for ; n > 0; n /= 2 {
-		lsb := n % 2
-		result = strconv.Itoa(lsb) + result
-	}
-	return result
-}
+// func convertToBin(n int) string {
+// 	result := ""
+// 	for ; n > 0; n /= 2 {
+// 		lsb := n % 2
+// 		result = strconv.Itoa(lsb) + result
+// 	}
+// 	return result
+// }
 
-func printFile(filename string) {
-	file, err := os.Open(filename)
-	if err != nil {
-		panic(err)
-	}
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		fmt.Println(scanner.Text())
-	}
-}
+// func printFile(filename string) {
+// 	file, err := os.Open(filename)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	scanner := bufio.NewScanner(file)
+// 	for scanner.Scan() {
+// 		fmt.Println(scanner.Text())
+// 	}
+// }
 
-func main() {
-	fmt.Println(
-		convertToBin(5),  // 101
-		convertToBin(13), // 1101
-		convertToBin(72387885),
-		convertToBin(0),
-	)
-	printFile("abc.txt")
-}
+// func main() {
+// 	fmt.Println(
+// 		convertToBin(5),  // 101
+// 		convertToBin(13), // 1101
+// 		convertToBin(72387885),
+// 		convertToBin(0),
+// 	)
+// 	printFile("abc.txt")
+// }
